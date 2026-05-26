9244004cc6830ddb37a3928290d6b94347fda6a6
Luciano Rampanelli <rampanelli@gmail.com>
2026-05-26 14:59:56 -0300

    Fix: restore getFilteredAndSorted method, v1.01B133

--- interface/src/containers/About.js
+++ interface/src/containers/About.js
@@ -52,7 +52,7 @@ class About extends Component {
               rjwats/esp8266-react
             </Typography>
             <div className={classes.version}>
-              BrewANO v1.01B132
+              BrewANO v1.01B133
             </div>
           </CardContent>
           <CardActions>
