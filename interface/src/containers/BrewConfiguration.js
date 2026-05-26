2024b6b63ec48ffa8ed397fd173d2495b52d4ad9
Luciano Rampanelli <rampanelli@gmail.com>
2026-05-26 14:29:16 -0300

    Restore import button in BrewConfiguration, v1.01B129

--- interface/src/containers/BrewConfiguration.js
+++ interface/src/containers/BrewConfiguration.js
@@ -3,8 +3,14 @@ import AppLayout from '../components/AppLayout';
 import MashSettings from './MashSettings';
 import BoilSettings from './BoilSettings';
 import BrewSettings from './BrewSettings'
+import BeerSmithImport from '../components/BeerSmithImport'
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import IntText from '../components/IntText'
 import { PageView, initGA } from '../components/Tracking'
 
@@ -13,6 +19,7 @@ class BrewConfiguration extends Component {
     super();
     this.state = {
       selectedTab: 'MashSettings',
+      importOpen: false,
     }
   }
 
@@ -22,20 +29,44 @@ class BrewConfiguration extends Component {
   }
 
   handleTabChange = (event, selectedTab) => {
-    this.setState({
-      selectedTab: selectedTab
-    })
+    this.setState({ selectedTab })
+  }
+
+  handleImportOpen = () => {
+    this.setState({ importOpen: true });
+  }
+
+  handleImportClose = () => {
+    this.setState({ importOpen: false });
   }
 
   render() {
-    const { selectedTab } = this.state
+    const { selectedTab, importOpen } = this.state
     return (
       <AppLayout sectionTitle="Brew Settings">
-        <Tabs value={selectedTab} onChange={this.handleTabChange} indicatorColor="secondary" textColor="contrastText" fullWidth scrollable>
-          <Tab value="MashSettings" label={<IntText text="Mash" />} />
-          <Tab value="BoilSettings" label={<IntText text="Boil" />} />
-          <Tab value="BrewSettings" label={<IntText text="BrewSettings.Settings" />} />
-        </Tabs>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Tabs value={selectedTab} onChange={this.handleTabChange} indicatorColor="secondary" textColor="contrastText" fullWidth scrollable style={{ flex: 1 }}>
+            <Tab value="MashSettings" label={<IntText text="Mash" />} />
+            <Tab value="BoilSettings" label={<IntText text="Boil" />} />
+            <Tab value="BrewSettings" label={<IntText text="BrewSettings.Settings" />} />
+          </Tabs>
+          <Button size="small" onClick={this.handleImportOpen} style={{ margin: '0 12px', whiteSpace: 'nowrap', minWidth: 'auto' }}>
+            <CloudUploadIcon style={{ marginRight: 4, fontSize: 18 }} />
+            <IntText text="Import.Button" />
+          </Button>
+        </div>
+        <Dialog open={importOpen} onClose={this.handleImportClose} maxWidth="md" fullWidth>
+          <DialogTitle><IntText text="Import.Title" /></DialogTitle>
+          <DialogContent>
+            <BeerSmithImport
+              onCancel={this.handleImportClose}
+              onImported={() => {
+                this.handleImportClose();
+                window.location.reload();
+              }}
+            />
+          </DialogContent>
+        </Dialog>
         {selectedTab === "MashSettings" && <MashSettings />}
         {selectedTab === "BoilSettings" && <BoilSettings />}
         {selectedTab === "BrewSettings" && <BrewSettings />}
